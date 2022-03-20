57454295fe45303ece84849a43c2ee77cec0ca3c
Muhammed-Fuhad <itsmuhammedfuhad@gmail.com>
2022-03-20 18:05:42 +0530

    added cookies

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,3 +80,38 @@ function countdown() {
 }
 let i = setInterval(countdown, 1000);
 countdown();
+
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your Email:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 1000);
+     }
+  }
+}
